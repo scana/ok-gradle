235300e68dcfc34ab6152cd7ae21a87b723f0772
Marcin Robaczyński <scanarch@gmail.com>
2020-01-12 22:04:25 +0100

    Use newer Gradle config file API instead of deprecated one

--- src/me/scana/okgradle/data/AddDependencyUseCase.kt
+++ src/me/scana/okgradle/data/AddDependencyUseCase.kt
@@ -1,12 +1,14 @@
 package me.scana.okgradle.data
 
-import com.android.tools.idea.gradle.dsl.api.GradleBuildModel
+import com.android.SdkConstants
+import com.android.tools.idea.gradle.dsl.api.ProjectBuildModel
 import com.android.tools.idea.gradle.dsl.api.dependencies.ArtifactDependencySpec
 import com.android.tools.idea.gradle.util.GradleUtil
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.util.ui.TextTransferable
@@ -19,9 +21,9 @@ class AddDependencyUseCase(
 ) {
 
     fun addDependency(module: Module, artifact: Artifact) {
-        val buildGradleFile = GradleUtil.getGradleBuildFile(module)
+        val buildGradleFile = findGradleFile(module)
         buildGradleFile?.let { gradleFile ->
-            val gradleBuildModel = GradleBuildModel.parseBuildFile(gradleFile, project, module.name)
+            val gradleBuildModel = ProjectBuildModel.get(project).getModuleBuildModel(gradleFile)
             val dependencies = gradleBuildModel.dependencies()
             val dependencySpec = ArtifactDependencySpec.create(artifact.name, artifact.groupId, artifact.version)
             val dependencyStrategy = AddDependencyStrategyFactory().create(dependencySpec)
@@ -42,6 +44,11 @@ class AddDependencyUseCase(
         val dependencyStrategy = AddDependencyStrategyFactory().create(dependencySpec)
         CopyPasteManager.getInstance().setContents(TextTransferable(dependencyStrategy.getDependencyStatements(dependencySpec).joinToString("\n") as CharSequence))
         notifier.showDependenciesStatementCopiedMessage()
+    }
+
+    private fun findGradleFile(module: Module): VirtualFile? {
+        val buildGradleFile = GradleUtil.getGradleBuildFile(module)
+        return buildGradleFile ?: module.moduleFile?.parent?.findChild(SdkConstants.FN_BUILD_GRADLE_KTS)
     }
 
 }
